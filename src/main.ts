17256bb7a5f9648ba41f019cb318042c99e9ffc4
andresgalaz <andres.galaz@gmail.com>
2022-01-05 13:56:06 -0300

    Obtiene PORT desde el ambiente Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ?? 3000;
+  // const host = process.env.HOST || '0.0.0.0';
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
