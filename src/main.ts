eb63335b90a9e3ceabd337833885e9c5e5bcd380
andresgalaz <andres.galaz@gmail.com>
2022-01-05 14:07:06 -0300

    Se agrega configuracion heroku-postbuild

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
-  // const host = process.env.HOST || '0.0.0.0';
-
   const app = await NestFactory.create(AppModule);
+
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
