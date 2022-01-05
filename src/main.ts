bf910efad46887cae8e23d08a83cd10573e07070
andresgalaz <andres.galaz@gmail.com>
2022-01-05 16:11:28 -0300

    Se agrega configuracion CORS:TRUE

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
